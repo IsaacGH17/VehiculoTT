48ee02561395a80fbb80462a2fafa18aa31a1f4e
IsaacGH17 <igarciah1800@alumno.ipn.mx>
2026-06-16 05:46:21 -0600

    Añadiendo lógica de programación navegación semiautomática

--- VehiculoTT/Core/Inc/globals.h
+++ VehiculoTT/Core/Inc/globals.h
@@ -8,6 +8,24 @@
 #ifndef INC_GLOBALS_H_
 #define INC_GLOBALS_H_
 #include <stdint.h>
+#include "cmsis_os.h"
+
+/* Porcentaje PWM actual (0-100) */
 extern volatile uint16_t percentage_pulse;
+
+/* Estado de acople del par 1 de ruedas (A / Cerrado_Pin) */
 extern volatile uint8_t ruedas_abiertas;
+
+/* Estado de acople del par 2 de ruedas (A1 / Cerrado1_Pin) */
+extern volatile uint8_t ruedas1_abiertas;
+
+/* Handle de event flags para la tarea semiautomática */
+extern osEventFlagsId_t semiAutoEvtHandle;
+
+/* Flags de evento */
+#define EVT_OBSTACULO1   (1U << 0)
+#define EVT_OBSTACULO2   (1U << 1)
+#define EVT_STOP_SEMI    (1U << 2)
+#define EVT_START_SEMI   (1U << 3)
+
 #endif /* INC_GLOBALS_H_ */
--- VehiculoTT/Debug/Core/Src/motor_control.d
+++ VehiculoTT/Debug/Core/Src/motor_control.d
@@ -33,7 +33,19 @@ Core/Src/motor_control.o: ../Core/Src/motor_control.c \
  ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim.h \
  ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim_ex.h \
  ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_uart.h \
- ../Core/Inc/globals.h ../Core/Inc/main.h
+ ../Core/Inc/globals.h \
+ ../Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS_V2/cmsis_os.h \
+ ../Middlewares/Third_Party/FreeRTOS/Source/include/FreeRTOS.h \
+ ../Core/Inc/FreeRTOSConfig.h \
+ ../Middlewares/Third_Party/FreeRTOS/Source/include/projdefs.h \
+ ../Middlewares/Third_Party/FreeRTOS/Source/include/portable.h \
+ ../Middlewares/Third_Party/FreeRTOS/Source/include/deprecated_definitions.h \
+ ../Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM4F/portmacro.h \
+ ../Middlewares/Third_Party/FreeRTOS/Source/include/mpu_wrappers.h \
+ ../Middlewares/Third_Party/FreeRTOS/Source/include/task.h \
+ ../Middlewares/Third_Party/FreeRTOS/Source/include/list.h \
+ ../Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS_V2/cmsis_os2.h \
+ ../Core/Inc/main.h
 ../Core/Inc/motor_control.h:
 ../Core/Inc/main.h:
 ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h:
@@ -70,4 +82,15 @@ Core/Src/motor_control.o: ../Core/Src/motor_control.c \
 ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim_ex.h:
 ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_uart.h:
 ../Core/Inc/globals.h:
+../Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS_V2/cmsis_os.h:
+../Middlewares/Third_Party/FreeRTOS/Source/include/FreeRTOS.h:
+../Core/Inc/FreeRTOSConfig.h:
+../Middlewares/Third_Party/FreeRTOS/Source/include/projdefs.h:
+../Middlewares/Third_Party/FreeRTOS/Source/include/portable.h:
+../Middlewares/Third_Party/FreeRTOS/Source/include/deprecated_definitions.h:
+../Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM4F/portmacro.h:
+../Middlewares/Third_Party/FreeRTOS/Source/include/mpu_wrappers.h:
+../Middlewares/Third_Party/FreeRTOS/Source/include/task.h:
+../Middlewares/Third_Party/FreeRTOS/Source/include/list.h:
+../Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS_V2/cmsis_os2.h:
 ../Core/Inc/main.h:
