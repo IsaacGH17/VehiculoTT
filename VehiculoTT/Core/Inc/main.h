e70afc1d3710b8c07a14e42ca9bc41b45816fcac
IsaacGH17 <igarciah1800@alumno.ipn.mx>
2026-09-20 14:32:02 -0600

    Declare limit-switch confirmation processor

--- VehiculoTT/Core/Inc/main.h
+++ VehiculoTT/Core/Inc/main.h
@@ -3,22 +3,9 @@
   ******************************************************************************
   * @file           : main.h
   * @brief          : Header for main.c file.
-  *                   This file contains the common defines of the application.
-  ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2025 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
   ******************************************************************************
   */
 /* USER CODE END Header */
-
-/* Define to prevent recursive inclusion -------------------------------------*/
 #ifndef __MAIN_H
 #define __MAIN_H
 
@@ -26,39 +13,12 @@
 extern "C" {
 #endif
 
-/* Includes ------------------------------------------------------------------*/
 #include "stm32f4xx_hal.h"
 
-/* Private includes ----------------------------------------------------------*/
-/* USER CODE BEGIN Includes */
-
-/* USER CODE END Includes */
-
-/* Exported types ------------------------------------------------------------*/
-/* USER CODE BEGIN ET */
-
-/* USER CODE END ET */
-
-/* Exported constants --------------------------------------------------------*/
-/* USER CODE BEGIN EC */
-
-/* USER CODE END EC */
-
-/* Exported macro ------------------------------------------------------------*/
-/* USER CODE BEGIN EM */
-
-/* USER CODE END EM */
-
 void HAL_TIM_MspPostInit(TIM_HandleTypeDef *htim);
-
-/* Exported functions prototypes ---------------------------------------------*/
 void Error_Handler(void);
+void FinalesCarrera_Process(void);
 
-/* USER CODE BEGIN EFP */
-
-/* USER CODE END EFP */
-
-/* Private defines -----------------------------------------------------------*/
 #define Abierto_Pin GPIO_PIN_14
 #define Abierto_GPIO_Port GPIOC
 #define Abierto_EXTI_IRQn EXTI15_10_IRQn
@@ -108,12 +68,7 @@ void Error_Handler(void);
 #define Acople_Pin GPIO_PIN_5
 #define Acople_GPIO_Port GPIOB
 
-/* USER CODE BEGIN Private defines */
-
-/* USER CODE END Private defines */
-
 #ifdef __cplusplus
 }
 #endif
-
 #endif /* __MAIN_H */
